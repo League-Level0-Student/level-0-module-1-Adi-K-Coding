ca7e52daaeb0e18adf2b248110333c80d30df340
Adi-K-Coding <league@WTS20.attlocal.net>
2018-02-25 14:51:01 -0800

    Badgers

---
+++ src/Badgers.java
@@ -0,0 +1,18 @@
+
+public class Badgers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int bob = 0; bob < 2; bob++) {
+
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Badger");
+			}
+			for (int i = 0; i < 2; i++) {
+				System.out.println("Mushroom");
+			}
+
+			System.out.println("Snake");
+		}
+	}
+}
